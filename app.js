b7f9a57ab379fc3a6618158a218ac99a35d41032
Xavi <javierpacareu@gmail.com>
2018-02-22 12:41:16 -0300

    Auto stash before merge of "master" and "origin/master"

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import angular from 'angular';
 import myModule from './app/app.module.js';
 import mainController from './app/main-app/main-app.controller.js';
-import secondaryController from './app/secondary-app/secondary.controller.js';
+import secondaryController from './app/second-app/second.controller.js';
 
--- app/secondary-app/secondary.controller.js
+++
@@ -1,7 +0,0 @@
-import myModule from '../app.module.js';
-
-export default myModule.controller('SecondaryController',['$scope',function($scope){
-	console.log("Secondary Controller created!");
-	var self = this;
-	self.myModel = "Hello world from the other controller!";
-}]);
