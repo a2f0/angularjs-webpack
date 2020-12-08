08826d0e1097936e140c01e79c97d0f61328942a
Dan Sullivan <dansullivan@gmail.com>
2020-12-07 23:56:55 -0500

    upgrade webpack to 5.4

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack'); //to access built-in plugins
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const config = {
   entry: './app.js',
@@ -11,8 +10,15 @@ const config = {
   },
   module: {
     rules: [
-       { test: /\.css$/, use: ExtractTextPlugin.extract({fallback: "style-loader",use: "css-loader"})},
-       { test: /\.js$/,  loader: "babel-loader", exclude:/node_modules/, query: {presets:["env"]}},
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude:/node_modules/
+      },
     ]
   },
   // resolve:{
@@ -23,8 +29,6 @@ const config = {
     compress: true
   },
   plugins: [
-    // new webpack.optimize.UglifyJsPlugin(),
-    new ExtractTextPlugin("styles.css"),
     new HtmlWebpackPlugin({
       title:"Todo App BB & WP",
       // inject:"head",
